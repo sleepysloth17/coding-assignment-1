b52be27fd1474962e23bd1348dc9c20433aa1ae8
Your Name <you@example.com>
2024-11-05 11:38:23 +0100

    add customer list component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: CustomerListComponent }];
